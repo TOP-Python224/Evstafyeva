021f6fe231fb586b0ed50c4b055c310dd3703089
Gennadiy <renardes@gmail.com>
2022-10-13 15:44:35 +0500

    08.24: правки удобства читаемости кода

--- 08.24/105.py
+++ 08.24/105.py
@@ -1,11 +1,10 @@
 # Упражнение 105. Произвольные системы счисления
+
 def int2random(number1: int,
-               radix1 = '16'):
+               radix1='16'):
     """Преобразовывает числа из десятичной системы счисления в 2-, 8-, 16-ную.
-    
-    
-    Параметр основания произвольной системы счисления должен быть строго ключевым
-    со значением по умолчанию равным 16."""
+
+    Параметр основания произвольной системы счисления должен быть строго ключевым со значением по умолчанию равным 16."""
     if radix1 == '2':
         number2 = bin(number1)[2:]
         return number2
@@ -18,6 +17,25 @@ def int2random(number1: int,
     else:
         print('Error')
 
+
+def random2int(number3: str,
+               radix2='16'):
+    """Преобразовывает числа из 2-, 8-, 16-нoй в десятичную систему счисления.
+
+    Параметр основания произвольной системы счисления должен быть строго ключевым со значением по умолчанию равным 16."""
+    if radix2 == '2':
+        number4 = int(number3, 2)
+        return number4
+    if radix2 == '8':
+        number4 = int(number3, 8)
+        return number4
+    if radix2 == '16':
+        number4 = int(number3, 16)
+        return number4
+    else:
+        print('Error')  
+
+
 res1_1 = int2random(15, '2')
 res1_2 = int2random(15, '8')
 res1_3 = int2random(15)
@@ -29,25 +47,6 @@ print()
 # res1_1 = '1111'
 # res1_2 = '17'
 # res1_3 = 'f'
-
-def random2int(number3: str,
-               radix2 = '16'):
-    """Преобразовывает числа из 2-, 8-, 16-нoй в десятичноую систему счисления.
-    
-    
-    Параметр основания произвольной системы счисления должен быть строго ключевым
-    со значением по умолчанию равным 16."""
-    if radix2 == '2':
-        number4 = int(number3, 2)
-        return number4
-    if radix2 ==  '8':
-        number4 = int(number3, 8)
-        return number4
-    if radix2 == '16':
-        number4 = int(number3, 16)
-        return number4
-    else:
-        print('Error')  
 
 res2_1 = random2int('1010', '2')
 res2_2 = random2int('34', '8')
